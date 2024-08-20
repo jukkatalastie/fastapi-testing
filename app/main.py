2e0665d8059af6fe0a0be659b0bb506ecedd54e9
Jukka Talastie <jukka.talastie@protonmail.com>
2024-08-20 19:33:55 +0000

    push out to prod

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Pushing out to ubuntu"}
 
 app.include_router(post.router)
 app.include_router(user.router)
