51a8819718445e3e38a168b85a6ce13741727a9e
Jukka Talastie <jukka.talastie@protonmail.com>
2024-08-21 07:20:22 +0000

    final commit maybe

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"message": "Pushing out to ubuntu yippii"}
+    return {"message": "does this finally work?"}
 
 app.include_router(post.router)
 app.include_router(user.router)
